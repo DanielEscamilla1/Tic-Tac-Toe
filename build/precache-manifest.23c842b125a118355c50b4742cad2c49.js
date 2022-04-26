56b0e87744c5112719caec33252e7a62ca42ede8
Daniel Escamilla <descamilla128@gmail.com>
2022-04-25 22:50:51 -0400

    first commit

---
+++ build/precache-manifest.23c842b125a118355c50b4742cad2c49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4697e1b8a22798100a94c47cd61bce7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1986d47cab1d06660a31",
+    "url": "/static/css/main.d87de66a.chunk.css"
+  },
+  {
+    "revision": "746ecf42605c214d7d17",
+    "url": "/static/js/2.71cd5e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71cd5e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1986d47cab1d06660a31",
+    "url": "/static/js/main.0badd1d3.chunk.js"
+  },
+  {
+    "revision": "fb23d6b5906dd033e185",
+    "url": "/static/js/runtime-main.dec1aa00.js"
+  },
+  {
+    "revision": "88d4da8b2ed3cf7bb1929bc4c25d10ca",
+    "url": "/static/media/confetti.88d4da8b.gif"
+  },
+  {
+    "revision": "175c9daeab6543a7266db3c5211674de",
+    "url": "/static/media/r2h.175c9dae.png"
+  }
+]);
